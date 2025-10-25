ac857c4ea995904df996afcbef993fb4e1d86352
ANKU890 <84028454+ANKU890@users.noreply.github.com>
2025-10-26 01:27:49 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.tsx';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
-const root = ReactDOM.createRoot(rootElement);
+const root = ReactDOM.createRoot(Could not resolve "./components/SettingsModal" from "src/App.tsx");
 root.render(
   <React.StrictMode>
     <App />
